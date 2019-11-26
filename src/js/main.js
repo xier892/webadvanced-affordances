6a19c81c518201313e1b0f73fb71367704dd9804
Ricky Xie <xier892@newschool.edu>
2019-11-26 03:21:05 -0500

    replace vh units

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 const setMaximumCapacity = () => Math.ceil((window.screen.availWidth / getWidthOfElement('pill transformedWidthTest')) * ((document.body.offsetHeight * 0.8) / getWidthOfElement('pill transformedWidthTest')));
 
-const setViewportHeight = () => {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-};
-setViewportHeight();
+// const setViewportHeight = () => {
+//   const vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty('--vh', `${vh}px`);
+// };
+// setViewportHeight();
 
 const SETTINGS = {
   PRESCRIPTION_DOSE_MIN: 1,
@@ -18,10 +18,10 @@ window.addEventListener('contextmenu', (event) => {
 });
 
 window.addEventListener('resize', () => {
-  setViewportHeight();
+  // setViewportHeight();
   SETTINGS.PRESCRIPTION_QTY_MAX = setMaximumCapacity();
 });
 window.addEventListener('orientationchange', () => {
-  setViewportHeight();
+  // setViewportHeight();
   SETTINGS.PRESCRIPTION_QTY_MAX = setMaximumCapacity();
 });
